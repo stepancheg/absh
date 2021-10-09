c86aa7bf417d712d571354b3b1b7c092bce10129
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-10-09 22:36:21 +0100

    MemUsage type

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod ansi;
 mod bars;
 mod duration;
+mod mem_usage;
 mod number;
 mod numbers;
 mod run_log;
@@ -13,6 +14,7 @@ pub use bars::plot_halves_u64;
 pub use bars::plot_u64;
 pub use bars::PlotHighlight;
 pub use duration::Duration;
+pub use mem_usage::MemUsage;
 pub use number::Number;
 pub use numbers::Numbers;
 pub use run_log::RunLog;
---
+++ src/mem_usage.rs
@@ -0,0 +1,71 @@
+use crate::Number;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::iter::Sum;
+use std::ops::Add;
+use std::ops::Sub;
+
+#[derive(Copy, Clone, Default, PartialOrd, Ord, PartialEq, Eq)]
+pub struct MemUsage {
+    bytes: u64,
+}
+
+impl MemUsage {
+    pub fn mb(&self) -> u64 {
+        self.bytes / 1_000_000
+    }
+
+    pub fn mib(&self) -> u64 {
+        self.bytes >> 20
+    }
+}
+
+impl Add for MemUsage {
+    type Output = MemUsage;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        MemUsage {
+            bytes: self.bytes + rhs.bytes,
+        }
+    }
+}
+
+impl Sub for MemUsage {
+    type Output = MemUsage;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        MemUsage {
+            bytes: self.bytes.checked_sub(rhs.bytes).unwrap(),
+        }
+    }
+}
+
+impl Sum for MemUsage {
+    fn sum<I: Iterator<Item = MemUsage>>(iter: I) -> MemUsage {
+        MemUsage {
+            bytes: iter.map(|m| m.bytes).sum(),
+        }
+    }
+}
+
+impl Display for MemUsage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}MiB", self.mib())
+    }
+}
+
+impl Number for MemUsage {
+    fn div_usize(&self, rhs: usize) -> Self {
+        MemUsage {
+            bytes: self.bytes / (rhs as u64),
+        }
+    }
+
+    fn as_f64(&self) -> f64 {
+        self.bytes as f64
+    }
+
+    fn from_f64(f: f64) -> Self {
+        MemUsage { bytes: f as u64 }
+    }
+}
