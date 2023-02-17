909d7b9a88dd96f3cf50abce7dac338a04389599
Sean Gillespie <sean@swgillespie.me>
2023-02-16 22:01:46 -0800

    [windows] Make absh work on Windows
    2. `spawn_sh` shells out to `/bin/sh`; added an implementation that
       does the same to `powershell.exe`.
    
    With that, I got absh running on my Windows 11 machine:
    
    ```
    running test: A
    running script:
        echo hi
    hi
    A finished in 0.135 s, max rss 60 MiB
    
    running test: B
    running script:
        echo hi
    hi
    B finished in 0.136 s, max rss 60 MiB
    
    Time (in seconds):
    A: n=49 mean=0.138 std=0.005 se=0.000 min=0.133 max=0.170 med=0.136
    B: n=49 mean=0.137 std=0.004 se=0.000 min=0.134 max=0.168 med=0.136
    A: distr=[  ▁▃█▄▂▁▁▂ ▁   ▁                                        ]
    B: distr=[  ▂▄▇▅▄▂ ▁▂   ▁                                         ]
    B/A: 0.995 0.979..1.011 (95% conf)
    ```

--- src/sh.rs
+++ src/sh.rs
@@ -2,9 +2,18 @@ use std::process::Child;
 use std::process::Command;
 use std::process::Stdio;
 
+#[cfg(not(windows))]
 pub fn spawn_sh(script: &str) -> anyhow::Result<Child> {
     Ok(Command::new("/bin/sh")
         .args(&["-ec", &script])
+        .stdin(Stdio::null())
+        .spawn()?)
+}
+
+#[cfg(windows)]
+pub fn spawn_sh(script: &str) -> anyhow::Result<Child> {
+    Ok(Command::new("powershell.exe")
+        .args(&["-Command", &script])
         .stdin(Stdio::null())
         .spawn()?)
 }
