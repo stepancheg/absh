a51defc90c335c932329141313792e59f4fde82f
Stepan Koltsov <stepan.koltsov@gmail.com>
2024-05-27 19:42:04 +0100

    code cleanup

--- src/sh.rs
+++ src/sh.rs
@@ -2,18 +2,19 @@ use std::process::Child;
 use std::process::Command;
 use std::process::Stdio;
 
-#[cfg(not(windows))]
 pub fn spawn_sh(script: &str) -> anyhow::Result<Child> {
-    Ok(Command::new("/bin/sh")
-        .args(&["-ec", &script])
-        .stdin(Stdio::null())
-        .spawn()?)
-}
-
-#[cfg(windows)]
-pub fn spawn_sh(script: &str) -> anyhow::Result<Child> {
-    Ok(Command::new("powershell.exe")
-        .args(&["-Command", &script])
-        .stdin(Stdio::null())
-        .spawn()?)
+    #[cfg(not(windows))]
+    {
+        Ok(Command::new("/bin/sh")
+            .args(&["-ec", &script])
+            .stdin(Stdio::null())
+            .spawn()?)
+    }
+    #[cfg(windows)]
+    {
+        Ok(Command::new("powershell.exe")
+            .args(&["-Command", &script])
+            .stdin(Stdio::null())
+            .spawn()?)
+    }
 }
